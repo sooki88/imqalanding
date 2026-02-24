601f8784aae47e796eb4664977c31f2b174b1f64
sooki <shnam@onycom.com>
2026-02-24 15:44:15 +0900

    헤더의 로고 클릭시 스크롤 상단

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,10 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center px-4 md:px-9 h-16 fixed top-0 right-0 left-0 z-50 pointer-events-auto backdrop-blur-sm">
       {/* // <header className="flex justify-between items-center px-4 md:px-9 h-16 fixed top-0 right-0 left-0 z-50 pointer-events-auto [transform:translateZ(0)] [backface-visibility:hidden] will-change-transform backdrop-blur-sm"> */}
-      <Link href="/">
+      <Link
+        href="/"
+        onClick={() => window.scrollTo({ top: 0, behavior: "auto" })}
+      >
         <Image
           src="/imqa_logo_wht.svg"
           alt="IMQA logo"
