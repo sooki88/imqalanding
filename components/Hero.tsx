82da8a15c68ef4906b74a1569d9d4d14cc7c4b2b
sooki <shnam@onycom.com>
2026-02-25 17:22:13 +0900

    테스트용: hero에 threads 삭제

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import SplitText from "./reactbits/SplitText";
-import Threads from "./reactbits/Threads";
 
 export default function Hero() {
   const handleAnimationComplete = () => {
@@ -13,15 +12,7 @@ export default function Hero() {
     <section className="min-h-screen">
       {/* 배경 레이어 */}
 
-      <div className="absolute bottom-[8%] left-0 right-0 md:inset-0 z-0 mix-blend-screen">
-        <Threads
-          amplitude={2.5}
-          distance={0.1}
-          enableMouseInteraction={true}
-          color={[1, 1, 1]}
-        />
-        {/* <Threads color={[1, 1, 1]} amplitude={0.8} distance={0.15} maxDpr={1} /> */}
-      </div>
+      <div className="absolute bottom-[8%] left-0 right-0 md:inset-0 z-0 mix-blend-screen"></div>
 
       <div className="relative z-10 pointer-events-none flex flex-col items-center pb-[20%] md:pb-0">
         <h1 className="text-main-gradient text-4xl md:text-[clamp(56px,4.26vw+4.8px,64px)] font-semibold leading-[1.3] break-keep">
